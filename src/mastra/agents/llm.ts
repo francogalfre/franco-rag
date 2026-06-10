11e6ded182c1133a66c62d28cdf201e1d822e681
francogalfre <francogalfre.code@gmail.com>
2026-06-10 00:12:07 -0300

    feat(llm): implement fallback model

--- src/mastra/agents/llm.ts
+++ src/mastra/agents/llm.ts
@@ -1,3 +1,14 @@
 import { env } from "@/config"
 
-export const ragAgentModel = [{ model: env.LLM_MODEL, modelSettings: { maxOutputTokens: 600 } }]
+export const ragAgentModel = [
+    {
+        model: env.LLM_MODEL,
+        maxRetries: 2,
+        modelSettings: { maxOutputTokens: 600 }
+    },
+    {
+        model: "openrouter/openai/gpt-oss-120b:free",
+        maxRetries: 3,
+        modelSettings: { maxOutputTokens: 600 }
+    },
+]
